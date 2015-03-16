f733c32ad753d5a0aa8368b99ba1ef3bdc2c22e8
Michael Shen <dextrys@dextryss-Mac-Pro.local>
2015-03-16 15:36:08 +0800

    Initial Commit

---
+++ ChildPainted/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChildPainted
+//
+//  Created by Michael Shen on 15/3/16.
+//  Copyright (c) 2015年 Dextrys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
